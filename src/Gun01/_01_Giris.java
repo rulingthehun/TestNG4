337871174d4302ba3187576c003c5fb903147392
rulingthehun <114784395+rulingthehun@users.noreply.github.com>
2023-03-27 21:43:25 +0300

    Project started

---
+++ src/Gun01/_01_Giris.java
@@ -0,0 +1,10 @@
+package Gun01;
+
+import Utility.BaseDriverFirefox;
+import org.testng.annotations.Test;
+
+public class _01_Giris extends BaseDriverFirefox {
+    @Test
+    public void Test(){
+    }
+}
